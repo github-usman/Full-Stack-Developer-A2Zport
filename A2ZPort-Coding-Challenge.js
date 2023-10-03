b41ba87398e85a96c8c9d620cf472adf12f4c729
github-usman <usmantoyou@gmail.com>
2023-10-03 18:41:35 +0530

    challenge complete

---
+++ A2ZPort-Coding-Challenge.js
@@ -0,0 +1,58 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function logestWord(sentence) {
+  const words = sentence.split(' ');
+
+  let longestWord = '';
+  let maxVowelCount = 0;
+
+  for (const word of words) {
+    
+    const newWord = word.replace(/[^a-zA-Z]/gi, '');
+    const vowelCount = countVowelEachWord(newWord);
+
+    if (newWord.length > longestWord.length) {
+      longestWord = newWord;
+      maxVowelCount = vowelCount;
+    } else if (newWord.length === longestWord.length && vowelCount > maxVowelCount) {
+      longestWord = newWord;
+      maxVowelCount = vowelCount;
+    }
+  }
+
+  return longestWord;
+}
+
+function countVowelEachWord(word) {
+  const vowels = 'aeiouAEIOU';
+  let count = 0;
+
+  for (const char of word) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+rl.question('Enter a sentence =====> ', (input) => {
+  const result = logestWord(input);
+  console.log('Ans is ======>', result);
+  rl.close();
+});
+
+                                                    ////////// SAMPLE test Cases //////////
+ // (1)  “Smart people learn from everything and everyone, average people from their experience, stupid people already, have all the answers” (Socrates)
+//  experience
+
+
+////////////////////English-Alpha/////////////////////12/////////////////////////26////////////////////////26/////////////////////////////////48///////////////////////////////////43////////////////////////////////////////39////////////                                       
+// (2) hello Usman Ali Ansari are you ready for aaaaaaaaaa10    A7z-portcodingchallenge21,   A6z-portcodingchallengh21, A2------------------------portcodingchallenge20, ####a###$%#%#$%@#$@$#%@#$@#$@#$#@$#@$#@@#$1   125456456487845454dfdfds54654654654sds9
+
+////////Azportcodingchallenge                           reason :- (i.e. A2z-portcodingchallenge) as given scenario//////////
---
+++ README.md
@@ -0,0 +1,31 @@
+
+# Full Stack Developer @ A2ZPort
+
+### Coding challenge:
+
+Given a sentence with multiple lowercase words separated by spaces, write a Javascript function that finds and returns the longest word in the sentence. If there are multiple words of the same length, choose one that has the highest number of vowels. Ignore any character in the sentence that is not an English letter or a space. Find the most efficient way to achieve this.
+
+Sample input:
+
+“Smart people learn from everything and everyone, average people from their experience, stupid people already, have all the answers” (Socrates)
+
+Sample output:
+
+“experience”
+
+Explanation: 
+
+Longest words are “everything” and “experience”, but the second has the most vowels
+
+
+### Random Sample input:
+
+``` 
+hello Usman Ali Ansari are you ready for aaaaaaaaaa10    A7z-portcodingchallenge21,   A6z-portcodingchallengh21, A2------------------------portcodingchallenge20, ####a###$%#%#$%@#$@$#%@#$@#$@#$#@$#@$#@@#$1   125456456487845454dfdfds54654654654sds9
+```
+__Output should be__
+```
+Azportcodingchallenge  
+```         
+__Reason__ (i.e. A2z-portcodingchallenge) as given scenario
+
